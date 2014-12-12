305bf07be05bdd63d0249ecc922091d1ad17df35
xingnx <xingnxc@gmail.com>
2014-12-12 17:50:48 -0500

    Added main run loop

--- references/24/Game24.py
+++ references/24/Game24.py
@@ -1,5 +1,7 @@
 import sys, pygame
 import os
+
+#Initialize pygame
 pygame.init()
 
 class Game24(object):
@@ -10,14 +12,52 @@ class Game24(object):
     STATE_HELP = 3
     
     def __init__(self):
+        self.main()
+
+    #Create a display
+    def display(self):
         self.screenSize = (self.width, self.height) = (1024, 768)
+        self.screen = pygame.display.set_mode(self.screenSize)
+        
+    #Update the display and show the button
+    def update_display(self):
         self.backgroundColor = 252,90,90
+        self.screen.fill(self.backgroundColor)
+        btn = pygame.image.load("assets/game_icons/add.png")
+        btnrect = btn.get_rect()
+        self.screen.blit(btn, btnrect)
+        pygame.display.flip()
+        
+    def main(self):
+        self.display()
+        
+        while True:
+            self.update_display()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                
+        
+        #pygame.init()
+
+        #size = (width, height) = (1024, 768)
+
+        #backgroundColor = 252,90,90
+
+        #screen = pygame.display.set_mode(size)
+        #screen.fill(backgroundColor)
+        #pygame.display.flip()
+        
+        #start button
+        
+        #help button
         
         #pygame.mixer.pre_init(44100, -16, 1, 512*2)
-        pygame.display.init()
-        pygame.font.init()
+        #pygame.display.init()
+        #pygame.font.init()
         #pygame.mixer.init(44100)
         #pygame.mouse.set_visible(False)
+
         """
         width = pygame.display.Info().current_w
         height = pygame.display.Info().current_h
@@ -29,17 +69,6 @@ class Game24(object):
         """
         #TextureLoader.screenSize =screenSize
         #self.scnHelp = SceneHelp(screenSize)
-
-        self.screen = pygame.display.set_mode(self.screenSize)
-        self.screen.fill(self.backgroundColor)
-
-        btn = pygame.image.load("assets/game_icons/add.png")
-        btnrect = btn.get_rect()
-        self.screen.blit(btn, btnrect)
-
-        
-        pygame.display.flip()
-        
         """
         SoundManager.init()
         SoundManager.EVENT_MUSIC_BACKGROUND()
@@ -71,21 +100,7 @@ class Game24(object):
                 self.STATE_WIN_SCREEN: self.scnWin,
                 self.STATE_HELP:  self.scnHelp}
         """
-  
-    #def main(self):
-        #pygame.init()
 
-        #size = (width, height) = (1024, 768)
 
-        #backgroundColor = 252,90,90
-
-        #screen = pygame.display.set_mode(size)
-        #screen.fill(backgroundColor)
-        #pygame.display.flip()
-        
-        #start button
-        
-        #help button
-    
 game = Game24()
 #game.main()
