8bd1f6bc9754d59e9210eb905e2d02872a613139
Tom Conroy <tdc5536@rit.edu>
2014-12-12 22:37:00 -0500

    added fame state detection in main menu

--- references/24/Game24.py
+++ references/24/Game24.py
@@ -12,12 +12,18 @@ class Game24(object):
     STATE_WIN_SCREEN = 2
     STATE_HELP       = 3
 
+
+    # Main Screen Logo
+    logo     = pygame.image.load(os.path.join('assets','buttons','logo.png'))
+    logoRect = logo.get_rect()
+
     # Main Screen Buttons
     startBtn = pygbutton
     helpBtn  = pygbutton
     quitBtn  = pygbutton
 
     def __init__(self):
+        self.currentState = self.STATE_MENU
         self.main()
 
     #Create a display
@@ -25,19 +31,30 @@ class Game24(object):
         pygame.display.set_caption('XO 24')        
         self.screenSize = (self.width, self.height) = (1024, 768)
         self.screen     = pygame.display.set_mode(self.screenSize)
-        self.startBtn   = pygbutton.PygButton( (self.width/2-125, self.height/2-125, 250, 90), 'START', bgcolor=(252,90,90), fgcolor=(255,255,255) )
-        self.helpBtn    = pygbutton.PygButton( (self.width/2-125, self.height/2, 250, 90),     'HELP',  bgcolor=(252,90,90), fgcolor=(255,255,255) )
-        self.quitBtn    = pygbutton.PygButton( (self.width/2-125, self.height/2+125, 250, 90), 'QUIT',  bgcolor=(255,90,90), fgcolor=(255,255,255) )
+        # define main screen buttons (oh god so sloppy)
+        self.startBtn   = pygbutton.PygButton( (self.width/2-75, self.height/2-105, 150, 50), 'START', bgcolor=(252,90,90), fgcolor=(255,255,255) )
+        self.helpBtn    = pygbutton.PygButton( (self.width/2-75, self.height/2, 150, 50),     'HELP',  bgcolor=(252,90,90), fgcolor=(255,255,255) )
+        self.quitBtn    = pygbutton.PygButton( (self.width/2-75, self.height/2+105, 150, 50), 'QUIT',  bgcolor=(255,90,90), fgcolor=(255,255,255) )
+        
+
 
     #Update the display and show the menu buttons
     def update_display(self):
-        self.backgroundColor = 252,90,90
-        self.screen.fill(self.backgroundColor)
+        # On the main menu: Draw background, buttons, and logo.
+        if self.currentState == self.STATE_MENU:
+            self.backgroundColor = 252,90,90
+            self.screen.fill(self.backgroundColor)
+            self.startBtn.draw(self.screen)
+            self.helpBtn.draw(self.screen)
+            self.quitBtn.draw(self.screen)
+            self.screen.blit(self.logo, (self.width/2-141, 100) )
         
-        # draw btnz
-        self.startBtn.draw(self.screen)
-        self.helpBtn.draw(self.screen)
-        self.quitBtn.draw(self.screen)
+        # On the Game Screen:
+        if self.currentState == self.STATE_GAME: 
+            print("state is game!")
+
+        # on the Help screen:
+        
 
         # flip yo shiz
         pygame.display.flip()
@@ -51,47 +68,25 @@ class Game24(object):
                 if event.type == pygame.QUIT or 'click' in self.quitBtn.handleEvent(event):
                     pygame.quit()
                     sys.exit()
-                if 'click' in self.startBtn.handleEvent(event):
-                    print("start btn clicked!")
-                if 'click' in self.helpBtn.handleEvent(event):
-                    print("Help btn clicked!")
+
+                if self.currentState == self.STATE_MENU:
+                    if 'click' in self.startBtn.handleEvent(event):
+                        print("start btn clicked!")
+                        self.currentState = self.STATE_GAME
+                    if 'click' in self.helpBtn.handleEvent(event):
+                        print("Help btn clicked!")
+
+                if self.currentState == self.STATE_GAME:
+                    print("todo: handle game state")
+
+                if self.currentState == self.STATE_WIN_SCREEN:
+                    print("todo: handle game win state")
+
+                if self.currentState == self.STATE_HELP:
+                    print("todo: handle help state")
                 
-        #pygame.init()
-
-        #size = (width, height) = (1024, 768)
-
-        #backgroundColor = 252,90,90
-
-        #screen = pygame.display.set_mode(size)
-        #screen.fill(backgroundColor)
-        #pygame.display.flip()
         
-        #start button
-        
-        #help button
-        
-        #pygame.mixer.pre_init(44100, -16, 1, 512*2)
-        #pygame.display.init()
-        #pygame.font.init()
-        #pygame.mixer.init(44100)
-        #pygame.mouse.set_visible(False)
-
         """
-        width = pygame.display.Info().current_w
-        height = pygame.display.Info().current_h
-        
-        if(float(width)/float(height) == float(4)/float(3)):
-            screenSize = (width,height)
-        else:
-            screenSize = (800,600)
-        """
-        #TextureLoader.screenSize =screenSize
-        #self.scnHelp = SceneHelp(screenSize)
-        """
-        SoundManager.init()
-        SoundManager.EVENT_MUSIC_BACKGROUND()
-        
-        
         DrawHelper.init(screenSize[0],screenSize[1])
         self.myFont = pygame.font.Font(os.path.join('assets', 'Minecraftia.ttf') , 24)
         IcnTextBox.setFont( self.myFont)
