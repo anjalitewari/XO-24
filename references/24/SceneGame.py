96c8196e1e5998d6692e40bfaadeafb3f8843372
anjalitewari <axt6472@rit.edu>
2014-12-15 19:49:55 -0500

    Fixed game buttons

--- references/24/SceneGame.py
+++ references/24/SceneGame.py
@@ -30,14 +30,15 @@ class	SceneGame(SceneBasic):
 		s.bttnBack	=	KButton(10,	10,	39,	31,	s.textureIdButtonBack,True)
 		center     = HelperVec2.mult(screenSize, (.5,.5) )
 		
-		s.numberButton1	=	KButton(center[0]-100, center[0]-100, 60, 60,	s.numberButtonImg,True)
-		s.bttn1 = IcnTextBox(center[0]-100, center[0]-100, 60, 60, "#111111111")
-		s.numberButton2	=	KButton(center[0]-100, center[0], 60, 60,	s.numberButtonImg,True)
-		s.bttn2 = IcnTextBox(center[0]-100, center[0], 60, 60, "#2")
-		s.numberButton3	=	KButton(center[1]+100, center[1]+100, 60, 60,	s.numberButtonImg,True)
-		s.bttn3 = IcnTextBox(center[1]+100, center[1]+100, 60, 60, "#3")
+		s.numberButton1	=	KButton(center[0]-100, center[0]-200, 60, 60,	s.numberButtonImg,True)
+		s.bttn1 = IcnTextBox(center[0]-100, center[0]-200, 60, 60, "#1", color=(252,90,90))
+		s.numberButton2	=	KButton(center[0]-100, center[0]-100, 60, 60,	s.numberButtonImg,True)
+		s.bttn2 = IcnTextBox(center[0]-100, center[0]-100, 60, 60, "#2", color=(252,90,90))
+		s.numberButton3	=	KButton(center[1]+100, center[1]-100, 60, 60,	s.numberButtonImg,True)
+		s.bttn3 = IcnTextBox(center[1]+100, center[1]-100, 60, 60, "#3", color=(252,90,90))
 		s.numberButton4	=	KButton(center[1]+100, center[1], 60, 60,	s.numberButtonImg,True)
-		s.bttn4 = IcnTextBox(center[1]+100, center[1], 60, 60, "#4")
+		s.bttn4 = IcnTextBox(center[1]+100, center[1], 60, 60, "#4", color=(252,90,90))
+		s.numberButtons = [s.numberButton1, s.numberButton2, s.numberButton3, s.numberButton4]
 
 
 	def	registerEvent_menu(s,e):	s.EVENT_MENU.append(e)
@@ -92,9 +93,10 @@ class	SceneGame(SceneBasic):
 		return	False
 
 def	runGame():
-	#self.game	=	Game24()
-	self.digits	=	Game24.choose4()	#available	buttons	-	only	add	or	delete	in	run
+	self.digits	=	test.choose4()	#available	buttons	-	only	add	or	delete	in	run
+	#call initbutton and update values
 	self.equation	=	[]	#	contains	
+	#listen for events 
 	print	"in	rungame"
 	#call	each	##	button	with	digits	and	display
 	self.chk	=	self.ans	=	False
